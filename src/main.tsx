a2ef0246155661f4ffada4458a7937677bc0bf80
jeil <iamnardu123@gmail.com>
2025-03-17 13:10:57 +0800

    css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "../css/reset.css";
+import "../css/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
